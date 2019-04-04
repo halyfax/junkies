2ae50fa93a1af3e02f3367cd5fb9400f8b23f042
Halyfax <giovany.torres.799@gmail.com>
2019-04-04 18:58:02 -0500

    comit

--- junkies/app.js
+++ junkies/app.js
@@ -8,7 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
-
+const mongoose = require('mongoose');
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
@@ -26,6 +26,12 @@ app.use('/users', usersRouter);
 app.use(function(req, res, next) {
   next(createError(404));
 });
+
+
+mongoose.connect('mongodb://localhost:27017/DBJunkies',{ useNewUrlParser: true }).then(()=>{
+  console.log("conectado a mongodb")
+});
+
 
 // error handler
 app.use(function(err, req, res, next) {
